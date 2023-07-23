7f57189193b9e7b162d2fc433a41b90d64450c97
fahimtakin <fahim.tanzil@hotmail.com>
2023-07-23 18:35:06 +0600

    add documentation

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,4 +3,12 @@ export const resolvers = {
   Query: {
     jobs: () => getJobs(),
   },
+
+  Job: {
+    date: (job) => toIsoDate(job.createdAt),
+  },
 };
+
+function toIsoDate(value) {
+  return value.slice(0, "yyyy-mm-dd".length);
+}
